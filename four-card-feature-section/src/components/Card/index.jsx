6806c3ba98dae59c34aee164bb71f3b7d9949d43
Samuel Gomez <gomezapari20@gmail.com>
2025-02-10 11:34:14 -0500

    add cols-2 in sm responsive

--- four-card-feature-section/src/components/Card/index.jsx
+++ four-card-feature-section/src/components/Card/index.jsx
@@ -1,6 +1,6 @@
 const Card = (({ title, description, image, borderColor}) => {
   return (
-    <article className={`shadow-very-dark-blue/20 flex flex-col gap-8 self-center rounded-lg border-t-4 p-8 shadow-2xl ${borderColor}`}>
+    <article className={`shadow-very-dark-blue/20 flex flex-col justify-between gap-8 rounded-lg border-t-4 p-8 shadow-2xl ${borderColor}`}>
       <div>
         <h2 className="text-very-dark-blue mb-2 text-xl font-semibold">{title}</h2>
         <p className="text-grayish-blue text-sm">{description}</p>
--- four-card-feature-section/src/components/ContainerCards/index.jsx
+++ four-card-feature-section/src/components/ContainerCards/index.jsx
@@ -6,14 +6,14 @@ import IconCalculator from "/images/icon-calculator.svg"
 
 const ContainerCards = () => {
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 md:gap-8">
+    <div className="lg:*:self-center grid grid-cols-1 gap-6 sm:grid-cols-2 md:gap-8 lg:grid-cols-3">
       <Card 
         title="Supervisor"
         description="Monitors activity to identify project roadblocks"
         image={IconSupervisor}
         borderColor="border-cyan"
       />
-      <div className="grid grid-cols-1 gap-6 md:gap-8">
+      <div className="grid grid-cols-1 gap-6 sm:order-3 sm:col-span-full sm:grid-cols-2 md:gap-8 lg:order-none lg:col-span-1 lg:grid-cols-1">
         <Card 
           title="Team Builder"
           description="Scans our talent network to create the optimal team for your project"
